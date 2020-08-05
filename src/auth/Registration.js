b93c8da8b64dd201006b778c4b857498f57b87ee
Rob Mixon <mixonroberth@gmail.com>
2020-08-05 12:12:01 -0500

    got registration going and worked on login to no avail

--- src/auth/Registration.js
+++ src/auth/Registration.js
@@ -1,36 +1,54 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import LoginManager from "../modules/LoginManager";
 import './Login.css';
 
 
 const Register = props => {
-const [user, setUser] = useState({ email:"", password:"", image: ""})
+const [user, setUser] = useState([])
+const [info, setInfo] = useState({ email:"", password:""})
 const [isLoading, setIsLoading]= useState(false);
 
 const handleFieldChange = evt => {
-  const stateToChange = { ...user };
+  const stateToChange = { ...info };
   stateToChange[evt.target.id] = evt.target.value;
-  setUser(stateToChange);
+  setInfo(stateToChange);
 };
+
+const getUsers = () => {
+  LoginManager.getAll().then((response) => {
+    setUser(response.map(result => ({email: result.email})))
+  })
+}
+
+const checkEmail = () => {
+  let check=user.some((response) => response.email === info.email)
+  return check
+}
 
 const constructNewUser = evt => {
   evt.preventDefault();
   let password2= document.querySelector("#password2").value
-  if(user.email===""||user.password===""){
+
+  if(info.email===""||info.password===""){
     window.alert("Please fill all fields out before creating a new account")
-  } else if (user.password!== password2) {
+  } else if (info.password!== password2){
     window.alert("Your password does not match")
+  } else if(checkEmail()){
+    window.alert("The email already has an account, please sign in to your account")
   } else {
-    console.log(user, "it works")
-    user.image = "./userIcon-black.png"
+    console.log(user,info, "it works")
     setIsLoading(true);
-    sessionStorage.setItem("user", JSON.stringify(user))
-    props.setUser(user)
-    LoginManager.post(user)
-    .then(()=>props.history.push("/"));
+    sessionStorage.setItem("user", JSON.stringify(info))
+    props.setUser(info)
+    LoginManager.post(info)
+    .then(()=>props.history.push("/login"));
   }
 };
+
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
     <div className="loginBox">
